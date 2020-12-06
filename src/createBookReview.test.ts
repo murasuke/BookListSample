a527189ac557764f9f83ed98fc674f64f5059446
tkyk.niimura <tkyk.niimura@gmail.com>
2020-12-06 12:27:30 +0900

    6-4Jestを利用したユニットテストまで実装

---
+++ src/createBookReview.test.ts
@@ -0,0 +1,30 @@
+import $ from "jquery";
+import createBookReview from "./createBookReview";
+import {Review} from "./app";
+
+
+describe("createBookReview", () => {
+    const review: Review = {
+        id: 1,
+        username: "テストユーザー",
+        comment: "この本はとても面白かったし学びも多い1冊でした。",
+        like: 3
+    };
+    test("should return DOM element", () => {
+        document.body.innerHTML = `<ul>${createBookReview(review)}</ul>`;
+        expect($(".review__list__item").length).toBe(1);
+    });
+
+    test("ユーザー名の一致確認", () =>{
+        document.body.innerHTML = `<ul>${createBookReview(review)}</ul>`;
+        expect($(".review__list__item__name").text()).toBe(`${review.username}さんの感想・評価`);
+    });
+    test("レビューコメントの一致確認", () =>{
+        document.body.innerHTML = `<ul>${createBookReview(review)}</ul>`;
+        expect($(".review__list__item__comment").text()).toBe(`${review.comment}`);
+    });
+    test("いいねの数の一致確認", () =>{
+        document.body.innerHTML = `<ul>${createBookReview(review)}</ul>`;
+        expect($(".review__list__item__like__button").text()).toBe(`❤️ ${review.like}件`);
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+    mode: process.env.NODE_ENV || "development",
+    entry: "./src/app.ts",
+    output: {
+        filename: "main.js",
+        path: path.resolve( __dirname, "dist"),
+    },
+    resolve: {
+        extensions: ['.js', '.ts']
+      },
+    module:{
+        rules:[
+            {
+                test: /\.(j|t)s$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: "babel-loader",
+                    options:{
+                        presets:["@babel/preset-env","@babel/preset-typescript"]
+                    }
+                }
+            }
+        ]
+    },
+    devtool: "inline-source-map"
+};
